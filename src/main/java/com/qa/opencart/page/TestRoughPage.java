56419f3f9c3f97ebb4921033c757bc45d0cbb9f0
mani0411 <manioverasu0411@gmail.com>
2023-12-29 22:33:08 +0530

    Review changes has been added as per review

--- src/main/java/com/qa/opencart/page/TestRoughPage.java
+++ src/main/java/com/qa/opencart/page/TestRoughPage.java
@@ -1,5 +1,13 @@
 package com.qa.opencart.page;
 
+import org.openqa.selenium.By;
+
 public class TestRoughPage {
+	
+	private By cart = By.id("cart");
+	
+	public void getCart() {
+		System.out.println("Cart Page is ready");
+	}
 
 }
