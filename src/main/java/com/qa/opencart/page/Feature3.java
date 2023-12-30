62c81f6a34b1a72889a1e4e0b44e30024086af26
mani0411 <manioverasu0411@gmail.com>
2023-12-30 22:52:36 +0530

    Feature 3 added

--- src/main/java/com/qa/opencart/page/Feature3.java
+++ src/main/java/com/qa/opencart/page/Feature3.java
@@ -1,10 +1,10 @@
 package com.qa.opencart.page;
 
-public class Feature1 {
+public class Feature3 {
 	
 	public static void main(String[] args) {
 
-		System.out.println("F3");
+		System.out.println("F1");
 	}
 
 }
